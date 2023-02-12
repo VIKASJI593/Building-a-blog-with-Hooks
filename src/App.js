20ed50d89333ad93a8ba7c54888c5b79c7a05984
VIKAS KUMAR <kumar.vikas593@gmail.com>
2023-02-12 12:26:46 +0530

    hooks

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state ={email: ''}
+  }
+  handleChange = (e) => {
+    this.setState({
+      email:e.target.value
+    
+    })
+  }
+render(){
+  return(
+<div className="App" style={{padding:10}}>
+
+  <input
+  value={this.state.email}
+  onChange={this.handleChange}
+  />
+  <p>Email:{this.state.email}</p>
+</div>
+)
+   }
 }
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
