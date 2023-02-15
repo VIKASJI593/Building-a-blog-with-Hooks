88e40b8a6de7af2bc7c5e8ce4287e71c16e55132
VIKAS KUMAR <kumar.vikas593@gmail.com>
2023-02-15 11:55:22 +0530

    5-email-NAME-usestate-const

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 git remote -v
 
-git branch -M 4-email-NAME-usestate
+git branch -M 5-email-NAME-usestate-const
 
 git branch
 
 git add .
 
-git commit -m "4-email-NAME-usestate"
+git commit -m "5-email-NAME-usestate-const"
 
 
-git push -u origin 4-email-NAME-usestate
+git push -u origin 5-email-NAME-usestate-const
 
-git push -f -u origin 3-email-NAME
+git push -f -u origin 5-email-NAME-usestate-const
--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
 import React, { useState } from "react";
 
 function App() {
-  const emailState = useState("");
-  const email = emailState[0];
-  const setEmail = emailState[1];
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
 
-  const nameState = useState("");
-  const name = nameState[0];
-  const setName = nameState[1];
+  function handleChange(e) {
+    setEmail(e.target.value);
+  }
+  function handleChangeName(e) {
+    setName(e.target.value);
+  }
 
-function handleChange (e) {
-  setEmail(e.target.value);
-}
-function handleChangeName (e) {
-  setName(e.target.value);
-}
+  return (
+    <div className="App" style={{ padding: 10 }}>
+      <input value={email} onChange={handleChange} />
 
-return (
-  <div className="App" style={{ padding: 10 }}>
-    <input value={email} onChange={handleChange} />
-
-    <input value={name} onChange={handleChangeName} />
-    <p>Email:{email}</p>
-    <p>Name:{name}</p>
-  </div>
-);
+      <input value={name} onChange={handleChangeName} />
+      <p>Email:{email}</p>
+      <p>Name:{name}</p>
+    </div>
+  );
 }
 export default App;
